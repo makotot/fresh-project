9c8c872806005567847022729ce42d335d92e18d
makotot <makoto.ttn@gmail.com>
2022-07-27 01:15:00 +0900

    init

---
+++ routes/about.tsx
@@ -0,0 +1,11 @@
+/** @jsx h */
+import {h} from "preact"
+
+export default function AboutPage () {
+  return (
+    <main>
+      <h1>about</h1>
+      <p>this is about page.</p>
+    </main>
+  )
+}
